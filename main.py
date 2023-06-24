3cfbd3e95866b3d26a5d85cd1ecc95bf33c1a09f
Levi-31 <shuklasarthak045@gmail.com>
2023-06-24 23:26:24 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,92 @@
+
+
+
+
+import face_recognition
+import cv2
+import numpy as np
+import csv
+from datetime import datetime
+
+video_capture = cv2.VideoCapture(0)
+#Load known Faces
+
+sarthak_image= face_recognition.load_image_file("FACES/sarthak.jpg")
+sarthak_encoding = face_recognition.face_encodings(sarthak_image)[0]
+
+virat_image = face_recognition.load_image_file("FACES/virat.jpg")
+virat_encoding = face_recognition.face_encodings(virat_image)[0]
+
+rohit_image =face_recognition.load_image_file("FACES/rohit.png")
+rohit_encoding = face_recognition.face_encodings(rohit_image)[0]
+
+
+known_faces_encoding = [sarthak_encoding, virat_encoding , rohit_encoding]
+known_face_name= ["SARTHAK", "VIRAT","ROHIT"]
+
+#list of expected student
+student = known_face_name.copy()
+
+face_locations = []
+face_encodings=[]
+
+#Get the currrent date and time
+now = datetime.now()
+
+current_date= now.strftime("%d-%m-%Y")
+
+
+f= open(f"{current_date}.csv" , "w+" , newline="")
+
+lnwriter= csv.writer(f)
+
+while True:
+    _, frame = video_capture.read()
+    small_frame = cv2.resize(frame,(0,0), fx=0.25 , fy=0.25)
+    rgb_small_frame= cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+    #RECOGNISE FACES
+    face_locations= face_recognition.face_locations(rgb_small_frame)
+    face_encodings=face_recognition.face_encodings(rgb_small_frame,face_locations)
+
+    for face_encoding in face_encodings:
+        matches= face_recognition.compare_faces(known_faces_encoding,face_encoding)
+        face_distance = face_recognition.face_distance(known_faces_encoding,face_encoding)
+        best_match_index = np.argmin(face_distance)
+
+        if(matches[best_match_index]):
+            name=known_face_name[best_match_index]
+        #Adding the text 
+        if name in known_face_name:
+            font =cv2.FONT_HERSHEY_SIMPLEX
+            bottomLeftCornerOfText =(10,100)
+            fontScale= 1.5
+            fontColor=(255,0,0)
+            thickness=3
+            lineType= 2
+            cv2.putText(frame, name+" Present" , bottomLeftCornerOfText,font,fontScale,fontColor,thickness,lineType)
+
+            if name in student:
+                student.remove(name)
+                current_time = now.strftime("%H-%M-%S")
+                lnwriter.writerow([name,current_time])
+
+
+
+    cv2.imshow("Attandence" ,frame)
+    if cv2.waitKey(1) & 0xFF==ord("q"):
+        break
+
+
+video_capture.release()
+cv2.destroyAllWindows()
+f.close()
+
+
+
+
+
+
+
+
+
